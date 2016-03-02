d2a57fa1ac335095c7ab96769f3a15be8f532ef2
Shana L Moore <shana.lavina.moore@gmail.com>
2016-03-01 21:03:34 -0800

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient (recipe, ingredientName, ingredientAmount){
+    recipe[ingredientName] = ingredientAmount;
+    return recipe;
+}
+
+function removeIngredient (recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient (recipe, ingredient, ingredientAmount){
+  recipe[ingredient] = ingredientAmount;
+  return recipe;
+}
+
+function readRecipe (recipe) {
+  for (var key in recipe){
+  console.log("this recipe calls for " + recipe[key] + " of " + key)
+  }
+}
